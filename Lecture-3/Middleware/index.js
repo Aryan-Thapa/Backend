ca379f71375118182fb5cc033dfaf545c129411e
Aryan-Thapa <aryanthapa219@gmail.com>
2025-04-05 17:58:53 +0530

    Middleware in expressJS

---
+++ Lecture-3/Middleware/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Middleware 2");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello JI");
+});
+
+app.get("/profile", (req, res, next) => {
+  next(new Error("Profile not found"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
