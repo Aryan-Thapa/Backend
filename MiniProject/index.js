21e738624574af252e42de3cecb0a2caad0ae61d
Aryan-Thapa <aryanthapa219@gmail.com>
2025-04-09 23:54:31 +0530

    [mini project] Register Login Signout routes added

---
+++ MiniProject/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    return res.status(400).send("User already exists");
+  } else {
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        const newUser = await userModel.create({
+          username: username,
+          email: email,
+          password: hash,
+        });
+      });
+    });
+    let token = jwt.sign({ email: email, userId: newUser._id }, "shhhh");
+    res.cookie("token", token);
+    res.status(200).send("User registered successfully");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/profile", isLoggedIn, (req, res) => {
+  console.log(req.user);
+  res.send(req.user);
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email: email });
+  if (!user) {
+    res.status(400).send("Entered email doesn't exists!!");
+  } else {
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (result) {
+        let token = jwt.sign({ email: email, userId: user._id }, "shhhh");
+        res.cookie("token", token);
+        res.status(200).send("User logged in successfully");
+      } else {
+        res.status(400).send("Entered Password is wrong");
+      }
+    });
+  }
+});
+
+app.get("/logout", isLoggedIn, (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/");
+});
+
+//isLoggedIn middleware
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "") res.send("You must be logged in!");
+  else {
+    let data = jwt.verify(req.cookies.token, "shhhh");
+    req.user = data;
+  }
+  next();
+}
+
+app.listen(3000, () => {
+  console.log("Server is running on PORT 3000");
+});
